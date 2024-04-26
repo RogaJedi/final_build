717fbc9f6f6ef1a216ae9168d91efd5b53076e44
RogaJedi <7654595@gmail.com>
2024-04-26 17:57:38 +0300

    commit

--- components/ProfileButton.tsx
+++ components/ProfileButton.tsx
@@ -3,7 +3,7 @@ import { redirect } from "next/navigation";
 
 export default async function ProfileButton() {
   const toProfile = async () => {
-    return redirect("/protected");
+    return redirect("/profile");
   };
 
   return (
